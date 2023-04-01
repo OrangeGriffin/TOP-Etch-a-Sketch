d9287570d15c8be23a910c4a4d656bb710d2fc06
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-01 10:19:07 -0500

    Add comment

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// const container = document.querySelector(".container");
-// const childDiv = document.createElement("div");
-
+// Create 256 childDiv elements
 for (i=1; i <=256; i++) {
     const container = document.querySelector(".container");
     const childDiv = document.createElement("div");
