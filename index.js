907416efb90432f9980a8dfaa1c99dea41fe0003
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-01 10:40:59 -0500

    Add hover and update background effect

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // Create 256 childDiv elements
-for (i=1; i <=256; i++) {
-    const container = document.querySelector(".container");
-    const childDiv = document.createElement("div");
-    container.appendChild(childDiv);
-    childDiv.className = ("child-div");
+for (i = 1; i <= 256; i++) {
+  const container = document.querySelector(".container");
+  const childDiv = document.createElement("div");
+  container.appendChild(childDiv);
+  childDiv.className = "child-div";
 }
+
+// Target nodelist of all child-divs
+const divTarget = document.querySelectorAll(".child-div");
+
+// Add event listener to each child-div
+// Update background-color to given child-div
+divTarget.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.style.backgroundColor = "black";
+  });
+});
