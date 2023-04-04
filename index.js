98424d9c4b4a079e6fd48ce4568309f4ac08cb05
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-04 18:33:04 -0500

    Extra credit part 2 working

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let opacity = 0.1;
+let enterCount = 0;
+
 // Create 256 childDiv elements
 for (i = 1; i <= 256; i++) {
   const container = document.querySelector(".container");
@@ -12,73 +15,84 @@ const divTarget = document.querySelectorAll(".child-div");
 // Add event listener to each child-div
 // Update background-color to given child-div
 divTarget.forEach((item) => {
-  item.addEventListener("mouseover", () => {
+  item.addEventListener("mouseover", (event) => {
     // Working for updating color to black
-   // item.style.backgroundColor = "black";
+    //event.target.style.backgroundColor = "black";
 
-   item.style.backgroundColor = randomRGB();
+    //Extra Credit Part 1: Working
+    //item.style.backgroundColor = randomRGB();
+
+    // Extra Credit Part 2: In Progress
+    let style = getComputedStyle(event.target);
+    //console.log(style.opacity);
+    let opacity = Number(style.opacity)
+    let newOpacity = updateOpacity(opacity);
+    item.style.opacity = newOpacity;
   });
 });
 
 // Create event listener for resize button
 // Assign the entered value to newSize
-const changeGridSizeBtn = document.querySelector("#change-grid-size")
-changeGridSizeBtn.addEventListener('click', () => {
-    let newSize = prompt("Enter Number up to 100")
-    clearGrid();
-    resizeGrid(newSize)
-})
+const changeGridSizeBtn = document.querySelector("#change-grid-size");
+changeGridSizeBtn.addEventListener("click", () => {
+  let newSize = prompt("Enter Number up to 100");
+  clearGrid();
+  resizeGrid(newSize);
+});
 
-const resizeGrid = function(newSize) {
-    //console.log("Size entered: ", newSize);
-    const inputSize = Number(newSize);
-    const iterator = inputSize*inputSize;
-    //console.log("Iterator: ", iterator);
-    const sideLength = ((600/inputSize) - 2) + "px";
-    //console.log("Side length: ", sideLength);
+const resizeGrid = function (newSize) {
+  //console.log("Size entered: ", newSize);
+  const inputSize = Number(newSize);
+  const iterator = inputSize * inputSize;
+  //console.log("Iterator: ", iterator);
+  const sideLength = 600 / inputSize - 2 + "px";
+  //console.log("Side length: ", sideLength);
 
-    // Create new childDivs based on given input
-    for (i = 1; i <= iterator; i++) {
-        const container = document.querySelector(".container");
-        const childDiv = document.createElement("div");
-        container.appendChild(childDiv);
-        childDiv.className = "child-div";
-      }
+  // Create new childDivs based on given input
+  for (i = 1; i <= iterator; i++) {
+    const container = document.querySelector(".container");
+    const childDiv = document.createElement("div");
+    container.appendChild(childDiv);
+    childDiv.className = "child-div";
+  }
 
-      // Because the original childDivs were removed and new childDivs
-      // were created, need to create a new target
-      const resizedDivTarget = document.querySelectorAll(".child-div");
-      // Give the new childDivs an updated height and width
-      resizedDivTarget.forEach((item) => {
-        item.style.width = sideLength;
-        item.style.height = sideLength;
-        item.style.backgroundColor = "#d5d5d5";
-      });
-}
+  // Because the original childDivs were removed and new childDivs
+  // were created, need to create a new target
+  const resizedDivTarget = document.querySelectorAll(".child-div");
+  // Give the new childDivs an updated height and width
+  resizedDivTarget.forEach((item) => {
+    item.style.width = sideLength;
+    item.style.height = sideLength;
+    item.style.backgroundColor = "#d5d5d5";
+  });
+};
 
 // Clear all existing childDivs
 const clearGrid = function () {
-    // Because the original childDivs were removed and new childDivs
-      // were created, need to create a new target
-    const clearDivTarget = document.querySelectorAll(".child-div");
-    clearDivTarget.forEach((item) => {
-        item.remove();
-      });
-}
+  // Because the original childDivs were removed and new childDivs
+  // were created, need to create a new target
+  const clearDivTarget = document.querySelectorAll(".child-div");
+  clearDivTarget.forEach((item) => {
+    item.remove();
+  });
+};
 
 // Creates randomb values for RGB and returns a string to pass through
 const randomRGB = function () {
-    const randomRed = Math.floor(Math.random() * 256).toString();
-    const randomGreen = Math.floor(Math.random() * 256).toString();
-    const randomBlue = Math.floor(Math.random() * 256).toString();
+  const randomRed = Math.floor(Math.random() * 256).toString();
+  const randomGreen = Math.floor(Math.random() * 256).toString();
+  const randomBlue = Math.floor(Math.random() * 256).toString();
 
-    const randomColor = "rgb(" + randomRed + "," + randomGreen + "," + randomBlue  + ")";
-    return randomColor;
-}
+  const randomColor =
+    "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+  return randomColor;
+};
 
- // Extra Crdit Part 2
-const percentageScalerTest = function() {
-  for (i=0; i<=100; i+=10) {
-      console.log(i/100);
+// Extra Credit Part 2
+const updateOpacity = function (opacity) {
+  if (opacity < 1) {
+    opacity = opacity + (10/100);
+    return opacity;
   }
 }
+
